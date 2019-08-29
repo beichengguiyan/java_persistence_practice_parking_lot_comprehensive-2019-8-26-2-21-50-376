6a24c4abe7de52cc8ea30e442fce58943c78548e
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-29 22:16:11 +0800

    Framework construction

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,12 @@
+CREATE TABLE parkinglot (
+  parkingLotID VARCHAR(36) PRIMARY KEY,
+  availablePositionCount int(4) NOT NULL，
+  count int(4) NOT NULL，
+  id VARCHAR(36)
+)；
+
 CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
+  id int(4) PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
   age   int(4) NOT NULL
 );
