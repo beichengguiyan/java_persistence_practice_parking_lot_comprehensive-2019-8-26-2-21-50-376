66c55be81c5cda1705a0d77d93d269449e0c4fea
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-29 22:50:40 +0800

    story2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,13 @@
-CREATE TABLE parkinglot (
-  parkingLotID VARCHAR(36) PRIMARY KEY,
-  availablePositionCount int(4) NOT NULL，
-  count int(4) NOT NULL，
-  id VARCHAR(36)
-)；
-
 CREATE TABLE employee (
   id int(4) PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
   age   int(4) NOT NULL
 );
+
+CREATE TABLE `parkinglot` (
+  `parkingLotID` INTEGER PRIMARY KEY,
+  `availablePositionCount` int(11) NOT NULL,
+ `count` int(11) NOT NULL,
+  `id` INTEGER NOT NULL
+);
+
